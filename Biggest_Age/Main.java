c4bc3024b85284fc626279a3c0a5e8109bc822cf
Luccas <luccas.henrique.vr@gmail.com>
2024-10-14 21:05:47 -0300

    Add method

--- Biggest_Age/Main.java
+++ Biggest_Age/Main.java
@@ -1,20 +1,24 @@
 public class Main {
-    public static void main(String[] args) {
-        int[] ages = {20,35,19,20,40,50};
+    public static void compareAges(int[] ages)
+    {
         int majorAge = 0;
 
         for(int i = 0; i < ages.length; i++){
             for(int j = ages.length - 1; j >= 0; j--)
             {
-               if (ages[i] != ages[j]){
-                   if(ages[i] > ages[j] && ages[i] > majorAge){
-                       majorAge = ages[i];
-                   }else if(ages[j] > majorAge){
-                       majorAge = ages[j];
-                   }
-               }
+                if (ages[i] != ages[j]){
+                    if(ages[i] > ages[j] && ages[i] > majorAge){
+                        majorAge = ages[i];
+                    }else if(ages[j] > majorAge){
+                        majorAge = ages[j];
+                    }
+                }
             }
         }
         System.out.println("The biggest age is "+ majorAge);
+    }
+    public static void main(String[] args) {
+        int[] ages = {20,35,19,20,40,50};
+        compareAges(ages);
     }
 }
